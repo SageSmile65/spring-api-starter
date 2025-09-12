da8843256c579052ec8a9393322432cb2a8de845
akshat <pathakakshat51@gmail.com>
2025-09-12 22:56:10 +0530

    returning json

--- src/main/java/com/codewithmosh/store/controllers/MessageController.java
+++ src/main/java/com/codewithmosh/store/controllers/MessageController.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.store.controllers;
 
+import com.codewithmosh.store.entities.Message;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
 
     @RequestMapping("/hello")
-    public String sayHello(){
-        return "hello";
+    public Message sayHello(){
+        return new Message("hello");
     }
 }
