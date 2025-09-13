4450af4a8bc65c0502cba47a04c2af6cf35161a3
sageSmile <pathakakshat51@outlook.com>
2025-09-13 13:48:37 +0530

    access each user by id

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.codewithmosh.store.controllers;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +12,19 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
-    @RequestMapping("/users")
+    @GetMapping
     public Iterable<User> getUser() {
         var userList = userRepository.findAll();
         return userList;
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable long id) {
+        var user = userRepository.findById(id).orElse(null);
+        return user;
     }
 }
