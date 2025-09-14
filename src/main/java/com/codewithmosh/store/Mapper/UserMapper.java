f09f0340905d138ac01d0fa7ad7213a800000637
sageSmile <pathakakshat51@outlook.com>
2025-09-14 11:53:31 +0530

    modifying response data

--- src/main/java/com/codewithmosh/store/Mapper/UserMapper.java
+++ src/main/java/com/codewithmosh/store/Mapper/UserMapper.java
@@ -3,8 +3,11 @@ package com.codewithmosh.store.Mapper;
 import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "createdAt",expression = "java(java.time.LocalDateTime.now())")
     UserDto userToUserDto(User user);
 }
--- src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -1,6 +1,11 @@
 package com.codewithmosh.store.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -9,4 +14,6 @@ public class UserDto {
     private Long id;
     private String name;
     private String email;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
 }
