20ec92e061867867984edace1eb1b6a159dab943
sageSmile <pathakakshat51@outlook.com>
2025-09-15 10:49:08 +0530

    create new repo function and use entity graph

--- src/main/java/com/codewithmosh/store/controllers/ProductController.java
+++ src/main/java/com/codewithmosh/store/controllers/ProductController.java
@@ -2,12 +2,13 @@ package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.Mapper.ProductMapper;
 import com.codewithmosh.store.dtos.ProductDto;
+import com.codewithmosh.store.entities.Product;
 import com.codewithmosh.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -17,11 +18,28 @@ public class ProductController {
     private final ProductMapper productMapper;
 
     @GetMapping
-    public Iterable<ProductDto> getAllProducts(){
-
-        var products =  productRepository.findAll().stream()
+    public Iterable<ProductDto> getAllProducts(
+            @RequestParam(required = false, name = "categoryId") Byte categoryId
+    ){
+        List<Product> products;
+        if(categoryId != null){
+            products = productRepository.findAllByCategory_Id(categoryId);
+        }
+        else{
+            products = productRepository.findAllWithCategory();
+        }
+        return products.stream()
                 .map(productMapper::toProductDto)
                 .toList();
-        return products;
+
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getProductById(@PathVariable Long id){
+        var product = productRepository.findById(id).orElse(null);
+        var productDto = productMapper.toProductDto(product);
+        if(productDto == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(productDto);
     }
 }
