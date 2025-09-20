414a553a0f638b893947f5c4a789488eb5e9178e
sageSmile <pathakakshat51@outlook.com>
2025-09-20 11:47:19 +0530

    set up jakarta validation

--- src/main/java/com/codewithmosh/store/dtos/RegisterUserRequest.java
+++ src/main/java/com/codewithmosh/store/dtos/RegisterUserRequest.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.store.dtos;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -11,6 +12,12 @@ public class RegisterUserRequest {
     @NotBlank
     @Size(max = 255,message = "Name must be less than 255 chararcters!")
     private String name;
+
+    @NotBlank
+    @Email(message = "Email must be valid")
     private String email;
+
+    @NotBlank
+    @Size(min = 6,max=25,message = "Password must be 6 to 25 characters long")
     private String password;
 }
