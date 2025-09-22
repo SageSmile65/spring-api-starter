a80d036b59dc8b7efe1900150073e091700480fd
sageSmile <pathakakshat51@outlook.com>
2025-09-22 19:54:52 +0530

    create cart and cart_items entities and establish relations

--- src/main/resources/db/migration/V2__create_carts_table.sql
+++ src/main/resources/db/migration/V2__create_carts_table.sql
@@ -4,4 +4,20 @@ create table carts
         primary key,
     date_created date       default (curdate())           null invisible
 );
+create table cart_items
+(
+    id         bigint        not null
+        primary key,
+    cart_id    binary(16)    not null,
+    product_id bigint        not null,
+    quantity   int default 1 null,
+    constraint cart_items_cart_product_unique
+        unique (cart_id, product_id),
+    constraint cart_items___fk
+        foreign key (product_id) references products (id)
+            on delete cascade,
+    constraint cart_items_carts_id_fk
+        foreign key (cart_id) references carts (id)
+            on delete cascade
+);
 
