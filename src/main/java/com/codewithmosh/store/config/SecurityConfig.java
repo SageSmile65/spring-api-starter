34fcc63d6eae5c46d1d2320fe87ef8c4df6f74ff
sageSmile <pathakakshat51@outlook.com>
2025-11-22 19:33:48 +0530

    fix dependency

--- src/main/java/com/codewithmosh/store/config/SecurityConfig.java
+++ src/main/java/com/codewithmosh/store/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.store.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
