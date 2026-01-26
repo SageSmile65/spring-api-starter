8731c4236355a762073b7e84948a5c07bff98d00
sageSmile <pathakakshat51@outlook.com>
2026-01-26 13:10:46 +0530

    add api to get current user in auth controller

--- src/main/java/com/codewithmosh/store/controllers/AuthController.java
+++ src/main/java/com/codewithmosh/store/controllers/AuthController.java
@@ -1,7 +1,9 @@
 package com.codewithmosh.store.controllers;
 
+import com.codewithmosh.store.Mapper.UserMapper;
 import com.codewithmosh.store.dtos.JwtResponse;
 import com.codewithmosh.store.dtos.LoginUserRequest;
+import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.repositories.UserRepository;
 import com.codewithmosh.store.services.JwtService;
 import lombok.AllArgsConstructor;
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +24,7 @@ import java.util.Map;
 @AllArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
+    private final UserMapper userMapper;
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
     private AuthenticationManager authenticationManager;
@@ -45,5 +49,17 @@ public class AuthController {
     public boolean validateToken(@RequestHeader("Authorization") String authHeader) {
         String token = authHeader.replace("Bearer ", "");
         return jwtService.validateToken(token);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> getUser(){
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var email = (String)authentication.getPrincipal();
+
+        var user =  userRepository.findByEmail(email).orElse(null);
+        if(user == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userMapper.userToUserDto(user));
     }
 }
